89a68eab711144107a433533a5b16d2f237625ce
Derek Demuro <ddemuro@gmail.com>
2024-10-20 04:21:12 -0400

    Added a lot of changes to support pre-post commands and to better support locked files

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 import os
 import sys
-from app.config_manager import ConfigurationManager
-from app.utils.filesystem_monitor import FilesystemMonitor
-from app.utils.rsync_manager import RsyncManager
+from utils.logs import Logger
+from utils.rsync import RsyncManager
+from utils.filesystem import FilesystemMonitor
+from utils.configuration import ConfigurationManager
+from utils.utils import validate_path
+
+DEFAULT_CONFIG_FILE = "/etc/fsrsync/config.json"
 
 
 class Application:
@@ -13,6 +17,13 @@ class Application:
         self.config_manager = ConfigurationManager(config_file)
         self.fs_monitor = FilesystemMonitor()
         self.destinations = []
+        self.config_manager.get_instance(config_file).load()
+        self.logger = Logger()
+        self.logger.set_level(
+            self.config_manager.get_instance(config_file)
+            .config.get("loglevel", "INFO")
+            .upper()
+        )
 
     def setup(self):
         """Set up the application by loading configuration and setting up rsync managers"""
@@ -21,56 +32,94 @@ class Application:
 
         # Set up rsync managers and inotify watchers for each destination
         for dest_config in destinations:
+            path = dest_config.get("path")
+            self.logger.info(f"Setting up destination: {path}")
+            if not validate_path(path):
+                self.logger.error(f"Invalid path: {path}, skipping destination...")
+                continue
+
             rsync_manager = RsyncManager(
-                destination=dest_config["destination"], options=dest_config["options"]
+                destination=dest_config["destination"],
+                options=dest_config["options"],
+                pre_sync_commands=dest_config.get("pre_sync_commands", []),
+                post_sync_commands=dest_config.get("post_sync_commands", []),
             )
             event_queue_limit = dest_config["event_queue_limit"]
-            paths = dest_config["paths"]
-            self.destinations.append(
-                {
-                    "rsync_manager": rsync_manager,
-                    "event_queue_limit": event_queue_limit,
-                    "event_count": 0,
-                }
-            )
+            destination_config = {
+                "rsync_manager": rsync_manager,
+                "event_queue_limit": event_queue_limit,
+                "event_count": 0,
+                "path": path,
+                "max_wait_locked": dest_config.get("max_wait_locked", 60),
+            }
 
-            for path_config in paths:
-                path = path_config["path"]
-                events = path_config["events"]
-                # Ensure we always monitor IN_OPEN and IN_CLOSE_WRITE
-                if "IN_CLOSE_WRITE" not in events:
-                    events.append("IN_CLOSE_WRITE")
-                if "IN_OPEN" not in events:
-                    events.append("IN_OPEN")
-                self.fs_monitor.add_watch(path, events)
-                rsync_manager.add_path(path)
+            events = dest_config["events"]
+            # Ensure we always monitor IN_OPEN and IN_CLOSE_WRITE
+            if "IN_CLOSE_WRITE" not in events:
+                events.append("IN_CLOSE_WRITE")
+            if "IN_OPEN" not in events:
+                events.append("IN_OPEN")
+            self.fs_monitor.add_watch(path, events)
+            rsync_manager.add_path(path)
+
+            self.destinations.append(destination_config)
 
     def run(self):
         """Run the application to monitor filesystem events and trigger rsync"""
         for event in self.fs_monitor.event_generator():
             type_names, path, filename = self.fs_monitor.handle_event(event)
-            print(f"Event detected: {type_names} on {path}/{filename}")
+            self.logger.info(f"Event detected: {type_names} on {path}/{filename}")
 
             # Update event counts and check queue limits for each destination
             for destination in self.destinations:
-                destination["event_count"] += 1
+                if destination is None:
+                    self.logger.error("Destination is None, skipping...")
+                    continue
+                self.logger.debug(f"Checking destination: {destination}")
+                if path.startswith(destination.get("path")):
+                    destination["event_count"] += 1
 
-                # Trigger rsync when event count reaches the limit
-                if destination["event_count"] >= destination["event_queue_limit"]:
-                    # Delay rsync if there are open files
-                    if self.fs_monitor.has_open_files():
-                        print("Open files detected, delaying rsync...")
-                    else:
-                        print(
+                    # Trigger rsync when event count reaches the limit
+                    if destination["event_count"] >= destination["event_queue_limit"]:
+                        # Get locked files in the path that have exceeded the max wait time
+                        should_exclude = (
+                            self.fs_monitor.check_if_locked_files_in_path_exceeded_wait(
+                                destination.get("path"), destination["max_wait_locked"]
+                            )
+                        )
+                        # Delay rsync if there are open files
+                        self.logger.info(
                             f"Event queue limit reached for destination {destination['rsync_manager'].destination}. Running rsync..."
                         )
-                        destination["rsync_manager"].run()
+                        # Should exclude
+                        exclude = None
+                        if not should_exclude:
+                            # Create a list in the format of --exclude={'/path/to/file1','/path/to/file2'}
+                            exclude = (
+                                "{'"
+                                + "','".join(
+                                    self.fs_monitor.get_locked_files_for_path(
+                                        destination.path
+                                    )
+                                )
+                                + "'}"
+                            )
+
+                        destination["rsync_manager"].run(exclude_list=exclude)
                         destination["event_count"] = 0
 
 
 """ Main entry point for the application """
 if __name__ == "__main__":
-    config_file = "config.json"
+    # If /etc/fsrsync/config.json exists, use it as the configuration file else use args
+    if validate_path(DEFAULT_CONFIG_FILE):
+        config_file = DEFAULT_CONFIG_FILE
+    else:
+        # Check if a configuration file is provided as an argument
+        if len(sys.argv) < 2:
+            print("Usage: python app.py <config_file>")
+            sys.exit(1)
+        config_file = sys.argv[1]
     app = Application(config_file)
     app.setup()
     app.run()
---
+++ app/utils/lockedmanager.py
@@ -0,0 +1,105 @@
+import os
+import time
+import threading
+from .wrappers import singleton
+from .utils import validate_path, is_file_open
+
+
+class LockedFile:
+    """Class to represent a locked file"""
+
+    def __init__(self, path, max_wait_locked, logger):
+        self.path = path
+        self.max_wait_locked = max_wait_locked
+        self.logger = logger
+        self.start_time = time.time()
+
+    def has_exceeded_wait(self):
+        """Check if the file has exceeded the max wait time"""
+        return time.time() - self.start_time > self.max_wait_locked
+
+    def is_open(self):
+        """Check if the file is open"""
+        return is_file_open(f"{self.path}")
+
+    def __str__(self):
+        return f"LockedFile(path={self.path}, max_wait_locked={self.max_wait_locked})"
+
+
+@singleton
+class PendingLocked:
+    """Class to keep track of pending locked files"""
+
+    def __init__(self, logger):
+        self.locked_files = []
+        self.exceeded_wait = []
+        self.logger = logger
+
+    def create_thread_to_check_locked_files_exceeded_wait(self):
+        """Create a thread to check if locked files have exceeded the max wait time"""
+        thread = threading.Thread(target=self.check_locked_files)
+        thread.daemon = True
+        thread.start()
+
+    def add_locked_file(self, path, max_wait_locked):
+        """Add a locked file to the list"""
+        self.locked_files.append(LockedFile(path, max_wait_locked, self.logger))
+
+    def remove_locked_file(self, path):
+        """Remove a locked file from the list"""
+        self.locked_files = [file for file in self.locked_files if file.path != path]
+
+    def has_locked_files(self):
+        """Check if there are locked files"""
+        return len(self.locked_files) > 0
+
+    def add_exceeded_wait(self, file):
+        """Add a file that has exceeded the max wait time"""
+        # If file is string, look for the LockedFile object in the list of locked files
+        if isinstance(file, str):
+            for locked_file in self.locked_files:
+                if locked_file.path == file:
+                    self.exceeded_wait.append(locked_file)
+        # Remove the LockedFile object from the list of locked files
+        self.locked_files = [locked_file for locked_file in self.locked_files if locked_file.path != file]
+
+    def has_exceeded_wait(self):
+        """Check if there are files that have exceeded the max wait time"""
+        return len(self.exceeded_wait) > 0
+
+    def check_locked_files(self):
+        """Check if any locked files have exceeded the max wait time"""
+        for file in self.locked_files:
+            exceeded_wait = time.time() - os.path.getmtime(f"{file.path}") > file.max_wait_locked
+            if is_file_open(f"{file.path}") and not exceeded_wait:
+                continue
+            if file not in self.exceeded_wait and exceeded_wait:
+                self.logger.info(f"File {file.path} has exceeded max wait time")
+                self.add_exceeded_wait(file)
+                self.remove_locked_file(file)
+
+    def check_locked_files_threaded(self):
+        """Check if any locked files have exceeded the max wait time"""
+        while True:
+            self.check_locked_files()
+            # Sleep 5 seconds before checking again
+            time.sleep(5)
+
+    def is_file_in_exceeded_wait(self, path):
+        """Check if a file is in the exceeded wait list"""
+        return path in [file.path for file in self.exceeded_wait]
+
+    def is_file_in_locked_files(self, filename):
+        """Check if a file is in the locked files list"""
+        return filename in [file.path for file in self.locked_files]
+
+    def clear_lockedfiles(self):
+        """Clear the list of locked files"""
+        self.locked_files.clear()
+
+    def clear_exceeded_wait(self):
+        """Clear the list of files that have exceeded the max wait time"""
+        self.exceeded_wait.clear()
+
+    def __str__(self):
+        return f"PendingLocked(locked_files={self.locked_files}, exceeded_wait={self.exceeded_wait})"
